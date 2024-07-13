74eb982e9d2e6a69fbdf10f6466500b2811d65f2
b1skeetz <damirk120404@gmail.com>
2024-07-13 14:05:43 +0500

    event works

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,14 +1,16 @@
-const categoryChoose = document.querySelector("select");
-let properties = [];
+const categoryChoose = document.getElementById("category");
+console.log(categoryChoose)
+
+// let properties = [];
 
 categoryChoose.onchange = function () {
+    alert('abc')
     const categoryId = categoryChoose.value;
     const url = `http://localhost:8080/products/create/properties?id=${categoryId}`;
-    let request = new XMLHttpRequest();
+    let request = new XMLHttpRequest(); // fetch или axios
     request.open("GET", url);
     request.onload = function () {
+        console.log(request.response)
         properties = request.response;
     };
 };
-
-alert("555");
