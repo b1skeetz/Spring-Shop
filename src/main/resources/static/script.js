f9a63aab8d4ebb5332a4157760b8ced5f8641938
Damir Kadyrzhanov <damirk120404@gmail.com>
2024-07-15 22:37:25 +0500

    little changes

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -1,8 +1,6 @@
 const categoryChoose = document.getElementById("category");
 console.log(categoryChoose)
 
-// let properties = [];
-
 categoryChoose.onchange = function () {
     const categoryId = categoryChoose.value;
     const url = `http://localhost:8080/products/create/properties?id=${categoryId}`;
